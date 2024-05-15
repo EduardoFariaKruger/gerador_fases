b8e3f965b0080b5b091e8f30953d2e1823015791
efk23 <efk23>
2024-05-15 15:59:22 -0300

    a

--- geradorFase.c
+++ geradorFase.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 struct coordenada
 {
@@ -68,6 +70,7 @@ void libera_matriz(int **matriz, int linhas, int colunas)
 
 void imprime_matriz(int **matriz, int linhas, int colunas)
 {
+	printf("\e[1;1H\e[2J");
 	if(matriz )
 	for (int i = 0; i < linhas; i++)
 	{
@@ -77,6 +80,7 @@ void imprime_matriz(int **matriz, int linhas, int colunas)
 		}
 		printf("\n");
 	}
+	sleep(1);
 }
 
 int gera(int a, int b)
@@ -112,17 +116,21 @@ struct coordenada move(struct coordenada pos_ini, int move)
 	{	
 		case 1:
 			b.y++;
+			break;
 		case 2:
 			b.y--;
+			break;
 		case 3:
 			b.x++;
+			break;
 		case 4:
-			b.x--;	
+			b.x--;
+			break;
 	}
 	return b;
 }
 
-int is_inside(struct coordenada vetor[], int tam, struct coordenada ponto)
+int isIn_Vector(struct coordenada vetor[], int tam, struct coordenada ponto)
 {
 	int c = 0;
 	for (int i = 0; i < tam; i++)
@@ -135,40 +143,78 @@ int is_inside(struct coordenada vetor[], int tam, struct coordenada ponto)
 	return c;
 }
 
-void gera_fase(struct coordenada *vetorCoord, int *vetTam, int **matriz, int vezes, int linhas, int colunas, struct coordenada pos_inicial)
+void gera_fase(int **matriz, int linhas, int colunas, struct coordenada pos_inicial, int qtdSalasCombate, int qtdSalasMercado, int qtdSalasQuimico, struct coordenada lajotas[], int *tam)
 {
-	if(vezes >= 0)
+	int random;
+	struct coordenada ponto;
+	if(qtdSalasCombate > 0 || qtdSalasMercado > 0 || qtdSalasQuimico > 0)
 	{
-		int aleat;
-		struct coordenada ponto;
-		aleat = gera(1, 4);
-		ponto = move(pos_inicial, aleat);
-		if(esta_dentro(ponto, linhas, colunas) && !is_inside(vetorCoord, *vetTam, ponto))
+		random = gera(1, 4);
+		ponto = move(pos_inicial, random);
+		if(esta_dentro(ponto, linhas, colunas) && !isIn_Vector(lajotas, *tam, ponto))
 		{
-			matriz[ponto.x][ponto.y] = 1;
-			vetorCoord[*vetTam] = ponto;
-			(*vetTam)++;
-			gera_fase(vetorCoord, vetTam, matriz, vezes-1, linhas, colunas, ponto);
+			if(qtdSalasCombate > 0)
+			{
+				//1 para sala de combate
+				matriz[ponto.x][ponto.y] = 1;
+				lajotas[*tam] = ponto;
+				(*tam)++;
+				printf("um\n");
+				imprime_matriz(matriz, linhas, colunas);
+				gera_fase(matriz, linhas, colunas, ponto, qtdSalasCombate - 1, qtdSalasMercado, qtdSalasQuimico, lajotas, tam);
+			}
+			else
+			{
+				if(qtdSalasMercado > 0)
+				{
+					//2 para sala de mercado
+					matriz[ponto.x][ponto.y] = 2;
+					lajotas[*tam] = ponto;
+					(*tam)++;
+					printf("dois\n");
+					imprime_matriz(matriz, linhas, colunas);
+					gera_fase(matriz, linhas, colunas, ponto, qtdSalasCombate, qtdSalasMercado - 1, qtdSalasQuimico, lajotas, tam);
+				}
+				else
+				{
+					//3 para sala do quimico
+					matriz[ponto.x][ponto.y] = 3;
+					lajotas[*tam] = ponto;
+					(*tam)++;
+					printf("tres\n");
+					imprime_matriz(matriz, linhas, colunas);
+					gera_fase(matriz, linhas, colunas, ponto, qtdSalasCombate, qtdSalasMercado, qtdSalasQuimico - 1, lajotas, tam); 
+				}
+			}
 		}
-		gera_fase(vetorCoord, vetTam, matriz, vezes, linhas, colunas, pos_inicial);
+		else
+		{
+			gera_fase(matriz, linhas, colunas, pos_inicial, qtdSalasCombate, qtdSalasMercado, qtdSalasQuimico, lajotas, tam);
+		}
 	}
+}	
+
+void imprime_vetor(struct coordenada vet[], int tam)
+{
+	for (int i = 0; i < tam; i++)
+	{
+		printf("(%d %d)  ", vet[i].x, vet[i].y);
+	}
+	printf("\n");
 }
 
 
 
 int main () 
 {
-	srand(100);
+	srand((unsigned)time(NULL));
 
-	//vetor de posicoes ocupadas
-	//alocar ele dinamicamente conforme mais tijolos forem percorridos
-	//declarar a matriz a ser usada
-	//alocar dinamicamente conforme as necessidades do usuario
 	int linhas;
 	int colunas;
 	int tam = 0;
 	int salas;
-	//int numeroDeSalas;
+	int mercados;
+	int quimicos;
 	int **matriz;
 	struct coordenada vazio;
 	vazio.x = 0;
@@ -179,13 +225,14 @@ int main ()
 	{
 		vetorCoord[i] = vazio;
 	}
-
-	printf("quantidade de linhas da matriz: ");
-	scanf("%d", &linhas);
-	printf("quantidade de colunas da matriz: ");
-	scanf("%d", &colunas);
-	printf("quantidade de salas na matriz: ");
+	printf("quantidade de salas de combate na matriz: ");
 	scanf("%d", &salas);
+	printf("quantidade de mercados na matriz: ");
+	scanf("%d", &mercados);
+	printf("quantidade de quimicos na matriz: ");
+	scanf("%d", &quimicos);
+	linhas = (salas + mercados + quimicos)*2;
+	colunas = (salas + mercados + quimicos)*2;
 	aleat.x = linhas / 2;
 	aleat.y = colunas / 2;
 	printf("%d\n", vetorCoord[0].x);
@@ -195,16 +242,15 @@ int main ()
 		preenche_matriz(matriz, linhas, colunas);
 		if(esta_dentro(aleat, linhas, colunas))
 		{
-			matriz[aleat.x][aleat.y] = 1;
+			matriz[aleat.x][aleat.y] = 4; //4 para representar onde o player ira surgir
 			vetorCoord[tam] = aleat;
 			tam++;
 		}
-		gera_fase(vetorCoord, &tam, matriz, salas, linhas, colunas, aleat);
+		gera_fase(matriz, linhas, colunas, aleat, salas, mercados, quimicos, vetorCoord, &tam);
 		imprime_matriz(matriz, linhas, colunas);
 	}
-	for (int p = 0; p < tam; p++)
-	{
-		printf("%d %d\n", vetorCoord[p].x, vetorCoord[p].y);
-	}
+	printf("\n");
+	imprime_vetor(vetorCoord, tam);
+	libera_matriz(matriz, linhas, colunas);
 	return 0;
 }
